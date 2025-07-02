4cb0655ac03e46842f6e7fd509a65c687eea8281
Saba Silagadze <sbsilagadze@gmail.com>
2025-07-02 20:11:21 +0400

    feat: reactors image-reveal animation
    Co-authored-by: Augustyn Glowacki <augustyndev@gmail.com>

--- src/components/animations/image-reveal/ImageReveal.tsx
+++ src/components/animations/image-reveal/ImageReveal.tsx
@@ -1,6 +1,6 @@
 import { ImageRevealImage } from "./components/ImageRevealImage";
+import { Content } from "./components/Content";
 import Image from "./assets/image.webp";
-import Arrow from "./assets/arrow.svg";
 
 export function ImageReveal() {
   return (
@@ -8,27 +8,7 @@ export function ImageReveal() {
       <div className="max-w-6xl mx-auto h-screen grid items-center grid-cols-2">
         <ImageRevealImage src={Image} alt="Hero Image" />
 
-        <div className="flex flex-col gap-4 ml-8 text-center">
-          <h2 className="text-lg font-bold text-green-700">This is label</h2>
-
-          <h1 className="text-5xl font-bold text-black leading-tight">
-            This text is reserved for title.
-          </h1>
-
-          <p className="text-xl text-gray-600">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. At magnam
-            explicabo eveniet enim.
-          </p>
-          <div className="mt-4">
-            <a
-              href="#wyjazdy"
-              className="inline-flex items-center px-6 py-3 text-white bg-green-700 rounded-lg hover:bg-green-800 transition-colors"
-            >
-              Learn More
-              <Arrow className="w-4 h-4 ml-2" />
-            </a>
-          </div>
-        </div>
+        <Content />
       </div>
     </div>
   );
